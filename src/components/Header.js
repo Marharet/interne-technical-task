028904c82fb5eb7166310cc98ac01f1f492048c2
Marharet <margo1702prachuk@gmail.com>
2024-03-16 18:43:24 +0200

    Header section done

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { CiSearch } from "react-icons/ci";
+import { GoHeart } from "react-icons/go";
+import { CiShoppingBasket } from "react-icons/ci";
+
+import "../css/Header.css"
+
+export default function Header() {
+    return (
+      <div>
+        <div className="header-container">
+            <div className="header-first">
+                <div className="help-box">
+                    <p>098 900 09 09</p>
+                    <p>Допомога</p>
+                </div>
+                <p>Увійти / Зареєструватися</p>
+            </div>
+
+            <div className="header-second">
+                <img src={require(`../img/Logo.png`)}></img>
+                <div className="nav-box">
+                    <p>новинки</p>
+                    <p>чоловіки</p>
+                    <p>жінки</p>
+                    <p>аксесуари</p>
+                    <p>акції</p>
+                </div>
+                <div className="search-box">
+                    <div className="search-line">
+                        <CiSearch className="search-icon"/>
+                    </div>    
+                    <GoHeart className="search-icon icon"/>
+                    <CiShoppingBasket className="search-icon icon"/>
+
+                </div>
+            </div>
+
+        </div>
+        
+      </div>
+    );
+  }
+  
+  
